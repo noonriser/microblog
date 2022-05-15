cabeb8a22f597e82d14db418a8647039e0dc6101
Will Daly <will.daly1@gmail.com>
2022-05-15 13:47:15 +0100

    Adding error handling - file and email.

--- run.sh
+++ run.sh
@@ -3,4 +3,19 @@ BASEDIR=$(dirname "$0")
 pip install -r $BASEDIR/requirements.txt
 # the below should not be needed with python-dotenv installed and .flaskenv file
 # export  FLASK_APP=$BASEDIR/microblog.py
+
+# For local testing of email, run the following in another terminal:
+# python -m smtpd -n -c DebuggingServer localhost:8025
+# Bring in this config for local testing of email
+# export MAIL_SERVER=localhost
+# export MAIL_PORT=8025
+
+# For running for real, update config.admins in init.py and bring in the following:
+# export MAIL_SERVER=smtp.googlemail.com
+# export MAIL_PORT=587
+# export MAIL_USE_TLS=1
+# export MAIL_USERNAME=<your-gmail-username>
+# export MAIL_PASSWORD=<your-gmail-password>
+# Extra config in Gmail may be needed to allow less secure apps
+# SendGrid seems like a nice alternative
 flask run
